6234894f362b2d6cc2ce8b44e30a3e3378e3e073
Saber Mesgari <saber.mesgari@gmail.com>
2022-11-28 00:30:28 +0330

    some code clean up

--- sessionttl.go
+++ sessionttl.go
@@ -1,33 +1,28 @@
 package ws
 
 import (
-	"os"
-	"os/signal"
 	"time"
 )
 
 func (w *wsserver) checkTTLofRecords() {
 	reversed := true
 	lowerBound := time.Date(1994, 1, 1, 0, 0, 0, 0, time.UTC)
-	upperBound := time.Now()
-
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
 
 	ticker := time.NewTicker(time.Second * 10)
 	defer ticker.Stop()
 
 	for range ticker.C {
-
+		upperBound := time.Now()
 		iterCh, err := w.wsmapTTL.BoundedIterCh(reversed, lowerBound, upperBound)
 		if err != nil {
 			continue
 		}
-		// defer iterCh.Close()
 
 		for rec := range iterCh.Records() {
 			w.onDelClient(rec.Key.(string))
 		}
+
+		iterCh.Close()
 	}
 
 }
